eb3c040c03a63f98b38e21936873d30ef86bde36
Clansty <i@gao4.pw>
2024-07-19 05:22:32 +0000

    添加节点延迟结果

--- custom_components/mihomo/sensor.py
+++ custom_components/mihomo/sensor.py
@@ -1,13 +1,19 @@
 import asyncio
+from datetime import datetime, timedelta
 import json
 import logging
+
+import aiohttp
 import voluptuous as vol
-from homeassistant.components.sensor import (PLATFORM_SCHEMA, SensorEntity)
-import homeassistant.helpers.config_validation as cv
 import websockets
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity, DataUpdateCoordinator)
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,19 +36,28 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     coordinator = MyCoordinator(hass, uri)
     await coordinator.async_config_entry_first_refresh()
-    async_add_entities([MyWebSocketSensor(f"{sensor_name}_up", coordinator, "up"), MyWebSocketSensor(
-        f"{sensor_name}_down", coordinator, "down")])
+    async_add_entities(
+        [
+            MyWebSocketSensor(f"{sensor_name}_up", coordinator, "up"),
+            MyWebSocketSensor(f"{sensor_name}_down", coordinator, "down"),
+        ]
+    )
+
+    ps_coor = ProxyStatusCoordinator(hass, uri)
+    await ps_coor.async_config_entry_first_refresh()
+    for proxy in ps_coor.data:
+        async_add_entities(
+            [
+                LastSpeedTestTimeSensor(proxy, ps_coor),
+                DelaySensor(proxy, ps_coor),
+            ]
+        )
 
 
 class MyCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, uri):
-        super().__init__(
-            hass,
-            _LOGGER,
-            name="Mihomo sensor",
-            always_update=True
-        )
-        self.uri = uri
+        super().__init__(hass, _LOGGER, name="Mihomo sensor", always_update=True)
+        self.uri = f"ws://{uri}/traffic"
         self.hass = hass
 
     async def async_config_entry_first_refresh(self):
@@ -63,7 +78,8 @@ class MyCoordinator(DataUpdateCoordinator):
                     await handle_message(message)
                 except websockets.ConnectionClosed:
                     _LOGGER.warning(
-                        "WebSocket connection closed. Retrying in 3 seconds.")
+                        "WebSocket connection closed. Retrying in 3 seconds"
+                    )
                     await asyncio.sleep(3)
                     websocket = await websockets.connect(self.uri)
 
@@ -84,4 +100,82 @@ class MyWebSocketSensor(CoordinatorEntity, SensorEntity):
     @callback
     def _handle_coordinator_update(self) -> None:
         self._attr_native_value = self.coordinator.data[self.up_down]
+        self.async_write_ha_state()
+
+
+class ProxyStatusCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, uri):
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Mihomo proxy status",
+            always_update=True,
+            update_interval=timedelta(seconds=15),
+        )
+        self.hass = hass
+        self.uri = f"http://{uri}/proxies"
+
+    async def _async_update_data(self):
+        async with (
+            aiohttp.ClientSession() as session,
+            session.get(self.uri) as response,
+        ):
+            return (await response.json())["proxies"]
+
+
+class LastSpeedTestTimeSensor(CoordinatorEntity, SensorEntity):
+    _attr_should_poll = False
+    _attr_device_class = "date"
+    _attr_has_entity_name = True
+
+    def __init__(self, sensor_name, coordinator):
+        super().__init__(coordinator, context=f"{sensor_name}.history")
+        self.sensor_name = sensor_name
+        self._attr_unique_id = f"{sensor_name}_last_speed_test_time"
+        self._attr_name = f"{sensor_name} 上次测速时间"
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        if not self.coordinator.data[self.sensor_name]:
+            return self.set_unavailble()
+        history = self.coordinator.data[self.sensor_name]["history"]
+        if not history:
+            return self.set_unavailble()
+        item = history[-1]
+        self._attr_native_value = datetime.fromisoformat(item["time"])
+        self.async_write_ha_state()
+
+    def set_unavailble(self):
+        self._attr_available = False
+        self.async_write_ha_state()
+
+
+class DelaySensor(CoordinatorEntity, SensorEntity):
+    _attr_should_poll = False
+    _attr_device_class = "duration"
+    _attr_has_entity_name = True
+    _attr_native_unit_of_measurement = "ms"
+    _attr_suggested_unit_of_measurement = "ms"
+
+    def __init__(self, sensor_name, coordinator):
+        super().__init__(coordinator, context=f"{sensor_name}.history")
+        self.sensor_name = sensor_name
+        self._attr_unique_id = f"{sensor_name}_delay"
+        self._attr_name = sensor_name
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        if not self.coordinator.data[self.sensor_name]:
+            return self.set_unavailble()
+        history = self.coordinator.data[self.sensor_name]["history"]
+        if not history:
+            return self.set_unavailble()
+        item = history[-1]
+        if not item["delay"]:
+            return self.set_unavailble()
+        self._attr_native_value = item["delay"]
+        self.async_write_ha_state()
+
+    def set_unavailble(self):
+        self._attr_available = False
         self.async_write_ha_state()
